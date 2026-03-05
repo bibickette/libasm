b192d4ba2491944abe7e9376f17386956315bf90
bibickette <imm.taly@gmail.com>
2026-03-05 16:47:00 +0100

    main for ft_write : write both function inside their own file so we juste need to make diff to see if there is any difference

--- tester/ft_write.c
+++ tester/ft_write.c
@@ -1,112 +1,85 @@
-// #include <stdio.h>
-// #include <string.h>
-// #include "libasm.h"
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include "libasm.h"
-#include <unistd.h>
 #include <errno.h>
 
-int main(void) {
-    char a[] = "bonjour";
+#define FT_WRITE 1
 
-    printf(" write returns %zd\n", write(1, a, strlen(a)));
-    perror(" write error");
-    printf(" my write returns %zd\n", ft_write(1, a, ft_strlen(a)));
-    perror(" my write error");
+void test_write(int fd, const void *data, size_t len, int id, ssize_t (*function)(int, const void *, size_t), int flag_write_or_ft) {
+    ssize_t ret = function(fd, data, len);
+    if (ret < 0) {
+        if(flag_write_or_ft == FT_WRITE)
+        {
+            printf("[❌] Test %d ft_write : error code = %ld\n", id, ret);
+            perror("ft_write");
+        }
+        else
+        {
+            printf("[❌] Test %d : error code = %ld\n", id, ret);
+            perror("write");
+        }
+    } else {
+        if(flag_write_or_ft == FT_WRITE)
+        {
+            printf("[✅] Test %d ft_write a écrit %zd octets\n", id, ret);
+        }
+        else
+        {
+            printf("[✅] Test %d a écrit %zd octets\n", id, ret);
+        }
+    }
+}
+
+void test_both_write(int fd_ft, int fd_regular, char *data, size_t len, int id){
+    test_write(fd_ft, data, len, id, ft_write, FT_WRITE);
+    test_write(fd_regular, data, len, id, write, WRITE);
+}
+
+
+int main(void) {
+    int id = 1;
+
+
+    int fd_ft = open("test_ft_write.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd_ft < 0) {
+        perror("open");
+        return 1;
+    }
+    int fd_regular = open("test_write.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd_regular < 0) {
+        close(fd_ft);
+        perror("open");
+        return 1;
+    }
+
+    test_both_write(fd_ft, fd_regular,"first test\n", ft_strlen("first_test\n"), id++);
+    test_both_write(fd_ft, fd_regular,"write extra memory", 25, id++);
+    test_both_write(fd_ft, fd_regular,"\nnot enough len", 11, id++);
+    test_both_write(fd_ft, fd_regular,"\ntest with null in the middle \n\0 of a sentence", ft_strlen("\ntest with null in the middle \n\0 of a sentence"), id++);
+
+    char *long_str = malloc(1024);
+    if (!long_str) {
+        perror("malloc");
+        return 1;
+    }
+    memset(long_str, 'A', 1023);
+    long_str[1023] = '\0';
+    test_both_write(fd_ft, fd_regular, long_str, 1024, id++);
+    free(long_str);
+
+    close(fd_ft);
+    close(fd_regular);
+
+    // test with stdin stdout stderr and invalid fd
+    test_both_write(0, 0, "is printed in stdin\n", ft_strlen("is printed in stdin\n"), id++);
+    test_both_write(1, 1, "is printed in stdout\n", ft_strlen("is printed in stdout\n"), id++);
+    test_both_write(1, 1, "is printed in stdout\n", ft_strlen("is printed in stdout\n"), id++);
+    test_both_write(-1, -1, "is printed in negative fd\n", ft_strlen("is printed in negative fd\n"), id++);
+    test_both_write(10, 10, "is printed in fd 10 that does not exist\n", ft_strlen("is printed in negative fd\n"), id++);
+    
 
     return 0;
 }
-
-// Compare deux fichiers octet par octet
-// int compare_files(const char *file1, const char *file2) {
-//     FILE *f1 = fopen(file1, "rb");
-//     FILE *f2 = fopen(file2, "rb");
-//     if (!f1 || !f2) {
-//         perror("fopen");
-//         return 0;
-//     }
-
-//     int res = 1;
-//     while (1) {
-//         int c1 = fgetc(f1);
-//         int c2 = fgetc(f2);
-//         if (c1 != c2) {
-//             res = 0;
-//             break;
-//         }
-//         if (c1 == EOF || c2 == EOF) break;
-//     }
-
-//     fclose(f1);
-//     fclose(f2);
-//     return res;
-// }
-
-// void test_write_case(const char *label, const char *data, size_t len) {
-//     const char *file_std = "out_std.txt";
-//     const char *file_ft = "out_ft.txt";
-
-//     // Ouvre deux fichiers en écriture
-//     int fd_std = open(file_std, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-//     int fd_ft  = open(file_ft,  O_WRONLY | O_CREAT | O_TRUNC, 0644);
-//     if (fd_std < 0 || fd_ft < 0) {
-//         perror("open");
-//         return;
-//     }
-
-//     // Écriture avec write()
-//     errno = 0;
-//     ssize_t ret_std = write(fd_std, data, len);
-//     int errno_std = errno;
-//     close(fd_std);
-
-//     // Écriture avec ft_write()
-//     errno = 0;
-//     ssize_t ret_ft = ft_write(fd_ft, data, len);
-//     int errno_ft = errno;
-//     close(fd_ft);
-
-//     printf("=== Test: %s ===\n", label);
-
-//     // Comparaison
-//     if (ret_std != ret_ft) {
-//         printf("[❌] Valeur de retour différente : write=%zd, ft_write=%zd\n", ret_std, ret_ft);
-//     } else if (errno_std != errno_ft) {
-//         printf("[❌] errno différent : write=%d, ft_write=%d\n", errno_std, errno_ft);
-//     } else if (!compare_files(file_std, file_ft)) {
-//         printf("[❌] Contenu écrit différent !\n");
-//     } else {
-//         printf("[✅] OK — %zd octets écrits identiques\n", ret_std);
-//     }
-
-//     printf("\n");
-// }
-
-// int main(void) {
-//     test_write_case("Chaîne simple", "Bonjour le monde\n", 17);
-//     test_write_case("Chaîne vide", "", 0);
-//     test_write_case("Chaîne binaire", "\x00\x01\x02HELLO\xff", 10);
-//     test_write_case("Chaîne avec \\0", "AB\0CD", 5);  // write() écrit bien les \0
-
-//     // Test sur un FD invalide
-//     errno = 0;
-//     ssize_t ret_std = write(-1, "Test", 4);
-//     int err_std = errno;
-
-//     errno = 0;
-//     ssize_t ret_ft = ft_write(-1, "Test", 4);
-//     int err_ft = errno;
-
-//     printf("=== Test: FD invalide ===\n");
-//     if (ret_std != ret_ft || err_std != err_ft) {
-//         printf("[❌] Différence sur FD invalide : write=%zd (errno=%d), ft_write=%zd (errno=%d)\n",
-//             ret_std, err_std, ret_ft, err_ft);
-//     } else {
-//         printf("[✅] OK — les deux ont échoué de la même manière (errno=%d)\n", err_std);
-//     }
-
-//     return 0;
-// }
